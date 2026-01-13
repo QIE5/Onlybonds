db416b7f54831ab450221df90f613a4f8ddfd513
Diyo234 <lkehinde2014@gmail.com>
2026-01-13 18:54:36 +0000

    Added CentroidCell, which stores the coordinates of the blob centres

--- Localisation/centroid.m
+++ Localisation/centroid.m
@@ -1,18 +1,21 @@
 simvid = VideoReader('simulation.mp4');
+numFrames = simvid.NumFrames;
+centroidCell = cell(numFrames, 1);
+frame1 = readFrame(simvid);
 
-
-frame = readFrame(simvid);
-frame = im2gray(frame);
-threshold = prctile(frame(:), 95)  % top 0.5% intensities, arbitrarily selected to include starburst
-bw = frame > threshold;
-cc = bwconncomp(bw, 8);
-stats = regionprops(cc, frame, 'WeightedCentroid');
-centroids = cat(1,stats.WeightedCentroid);
-
-
+for n = 1: numFrames;
+    frame = read(simvid, n);
+    frame = im2gray(frame);
+    threshold = prctile(frame(:), 99.5);  % top 0.5% intensities, arbitrarily selected to include starburst
+    bw = frame > threshold;
+    cc = bwconncomp(bw, 8);
+    stats = regionprops(cc, frame, 'WeightedCentroid');
+    centroidCell{n} = cat(1,stats.WeightedCentroid);
+end
+centroids = centroidCell{1};
 figure;
-imshow(frame);
+imshow(frame1);
 hold on
-plot(centroids(:,1),centroids(:,2),'b*')
+plot(centroids(:,1),centroids(:,2),'b*');
 hold off
 title('Frame 1');
