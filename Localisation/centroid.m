5abf8178c4bdc9b59ee10ca3bd377b7ede27e716
Diyo234 <lkehinde2014@gmail.com>
2026-01-13 22:34:05 +0000

    Added localised video

--- Localisation/centroid.m
+++ Localisation/centroid.m
@@ -1,7 +1,15 @@
 simvid = VideoReader('simulation.mp4');
+
+%% Code for producing video with localied MBs
+% localisedvid = VideoWriter("localised");
+% localisedvid.FrameRate = simvid.FrameRate;
+% open(localisedvid);
+% figure('Visible','off');
+
 numFrames = simvid.NumFrames;
 centroidCell = cell(numFrames, 1);
 frame1 = readFrame(simvid);
+
 
 for n = 1: numFrames;
     frame = read(simvid, n);
@@ -11,6 +19,13 @@ for n = 1: numFrames;
     cc = bwconncomp(bw, 8);
     stats = regionprops(cc, frame, 'WeightedCentroid');
     centroidCell{n} = cat(1,stats.WeightedCentroid);
+    %%
+    % imshow(frame); hold on
+    % plot(centroidCell{n}(:,1), centroidCell{n}(:,2), 'r+', 'MarkerSize',6)
+    % hold off
+    % 
+    % F = getframe(gca);
+    % writeVideo(localisedvid, F);
 end
 centroids = centroidCell{1};
 figure;
@@ -19,3 +34,6 @@ hold on
 plot(centroids(:,1),centroids(:,2),'b*');
 hold off
 title('Frame 1');
+
+% close(localisedvid);
+% close(gcf);
