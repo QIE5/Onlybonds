1f877b92d70f4f771f0319b8aa6bcc6628700eb1
QIE5 <kq223@ic.ac.uk>
2026-04-15 04:46:25 +0100

    Add files via upload
    new powerdoppler

--- powerdoppler.m
+++ powerdoppler.m
@@ -1,9 +1,5 @@
-% load('');
 
-N = min(2000, size(beamformed_data, 3));
-data = beamformed_data(:, :, 1:N);
-
-% SVDparameter
+%SVD parameter
 nCut = 4;
 nNoiseCut = 0;
 
@@ -13,17 +9,71 @@ data_filt = svdClutterFilter(data, nCut, nNoiseCut);
 %Power Doppler
 PD = sum(abs(data_filt).^2, 3);
 
-% convert to dB
+%convert to dB
 PD_dB = 10*log10(PD + eps);
 PD_dB = PD_dB - max(PD_dB(:));
 
 
+PD_dB = rot90(PD_dB, 2);
+
+
+pixelSize = 3.3879e-5;
+
+
+[rows, cols] = size(PD_dB);
+
+dx = pixelSize;
+dz = pixelSize;
+
+
+xAxis_mm = ((1:cols) - 0.5) * dx * 1e3;
+zAxis_mm = ((1:rows) - 0.5) * dz * 1e3;
+
+
 figure;
-imagesc(imaging_x_axis, imaging_z_axis, PD_dB, [-40 0]);
-axis image;
+imagesc(xAxis_mm, zAxis_mm, PD_dB, [-40 0]);
 set(gca, 'YDir', 'normal');
 colormap hot;
 colorbar;
-xlabel('x (m)');
-ylabel('z (m)');
-title(sprintf('SVD Power Doppler (nCut=%d, N=%d)', nCut, N));
+axis image off;
+
+figure;
+imagesc(xAxis_mm, zAxis_mm, PD_dB, [-40 0]);
+
+colormap hot;
+colorbar;
+axis image off;
+
+
+set(gca, 'YDir', 'normal');
+
+hold on;
+
+
+barLength_mm = 1;
+marginFrac   = 0.06;
+
+ax = gca;
+xl = xlim(ax);
+yl = ylim(ax);
+
+xRange = xl(2) - xl(1);
+yRange = yl(2) - yl(1);
+
+
+x0 = xl(1) + marginFrac * xRange;
+
+
+if strcmp(ax.YDir, 'normal')
+    y0 = yl(1) + marginFrac * yRange;
+else
+    y0 = yl(2) - marginFrac * yRange;
+end
+
+
+plot([x0, x0 + barLength_mm], [y0, y0], ...
+    'w', 'LineWidth', 4, 'Clipping', 'on');
+
+hold off;
+
+exportgraphics(gcf, 'power_doppler.png', 'Resolution', 300);
